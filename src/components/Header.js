c65110ad32c73533da81255474f037ced44adfe9
nihar kushwaha <niharkushwaha7@gmail.com>
2023-02-28 14:03:10 +0530

    v2

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="bg-[#171C1C] text-white font-bold text-xl h-20 md:w-[95%] mx-auto mb-10 p-6">
       <a href="/">
-        <span className="text-[#007CFE]">Easy</span>QR
+        QR<span className="text-[#007CFE]">Easy</span>
       </a>
     </div>
   );
