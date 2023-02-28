adf1c14948fde9308be7617a7184621a5407f019
nihar kushwaha <niharkushwaha7@gmail.com>
2023-02-28 14:17:38 +0530

    v2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,11 @@ export default function Footer() {
   return (
     <footer class="p-4 bg-white sm:p-6 dark:bg-gray-900">
       <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-      <div class="sm:flex sm:items-center sm:justify-between">
+      <div class="flex items-center justify-between">
         <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2023 Nihar Kushwaha . All Rights Reserved.
         </span>
-        <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
+        <div class="flex w-10 space-x-6 sm:justify-center sm:mt-0">
           <a
             href="https://github.com/Nihar-kush"
             class="text-gray-500 hover:text-gray-900 dark:hover:text-white"
